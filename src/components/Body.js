0eb9b1de833d7832c5b2a8c2831b8151c04651a7
James <james.rudd45@gmail.com>
2019-10-21 17:57:50 +1100

    Initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Body() {
+  return <p>This is the Body</p>;
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>ClickBits, the clicky clicky memory game</h1>
+      <p>Don't click a picture more than once</p>
+    </div>
+  );
+}
+
+export default Header;
