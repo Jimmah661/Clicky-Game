2cd086989cc7f203ebc67fa20bc03a9b251ef8ae
James <james.rudd45@gmail.com>
2019-10-26 11:38:46 +1100

    Updates

---
+++ precache-manifest.94bb7408bb691b9f8352b0a701f1b262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdb011b337dda428634194ce0277d2f",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "d4961d311e95002819f2",
+    "url": "/Clicky-Game/static/css/main.569b378f.chunk.css"
+  },
+  {
+    "revision": "2e34652ae451bffbad52",
+    "url": "/Clicky-Game/static/js/2.79a1a337.chunk.js"
+  },
+  {
+    "revision": "d4961d311e95002819f2",
+    "url": "/Clicky-Game/static/js/main.595961f0.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
